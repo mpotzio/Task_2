af09c32d7bfea5615f36c562d4309dcf12a7179d
stathis <stabotz@gmail.com>
2026-05-07 20:09:31 +0200

    Added tax function

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 def convert_usd_to_eur(usd):
     return usd * 0.9
+
+def calculate_tax(amount):
+    return amount * 0.2
---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import convert_usd_to_eur
+
+class TestApp(unittest.TestCase):
+    def test_convert(self):
+        self.assertEqual(convert_usd_to_eur(10), 9.0)
+
+if __name__ == '__main__':
+    unittest.main()
