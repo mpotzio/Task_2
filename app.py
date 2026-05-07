64d7231f3ab0792c170797fb8a5cd8597ede7ccd
dimitragr <dimigr2004@gmail.com>
2026-05-07 21:01:39 +0300

    added eur to usd conversion

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 def convert_usd_to_eur(usd):
     return usd * 0.9
+
+def convert_usd_to_eur(eur):
+    return eur * 1.1
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import convert_usd_to_eur, convert_eur_to_usd
+
+def test_convert_eur(self):
+    self.assertEqual(convert_eur_to_usd(10), 11.0)
